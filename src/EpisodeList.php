3a35209ffbb6eaaf58892ad364e8a6ebc880a647
Chris Throup <chris@throup.org.uk>
2015-06-26 16:07:44 +0100

    Added shell class definition for Throup\GrabRadio\EpisodeList

---
+++ src/EpisodeList.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Throup\GrabRadio;
+
+class EpisodeList {
+}
---
+++ tests/unit/EpisodeList_UnitTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Throup\GrabRadio;
+
+use PHPUnit_Framework_TestCase;
+
+class EpisodeList_UnitTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @test
+     */
+    public function instantiate() {
+        new EpisodeList();
+    }
+}
